c3af5b2380cdde904408c7a1bcb77ff499c3024e
heggria <bshengtao@gmail.com>
2022-06-03 12:59:44 +0800

    feat: upd py

---
+++ imageSizeChange.py
@@ -0,0 +1,25 @@
+from PIL import Image
+import os
+
+from utils import del_dictionary_file
+
+# 统一至指定图片大小
+# 居中、裁剪、缩放
+scaleSize = 1
+width = 512
+height = 512
+
+def img_size_change(source_folder : str, destination_folder : str):
+    del_dictionary_file(destination_folder)
+    # iterate over all files in source folder
+    for root, dirs, files in os.walk(source_folder):
+        for file_name in files:
+            file_path = os.path.join(root, file_name)
+            out_file_path = os.path.join(destination_folder, file_name)
+            im = Image.open(file_path)
+            if file_name.find('uniequip') > -1 or file_name.find('default') > -1 :
+                box = ((im.size[0] - 512) / 2, (im.size[1] - 512) / 2 , im.size[0] - (im.size[0] - 512) / 2, im.size[1] - (im.size[1] - 512) / 2 )
+                region = im.crop(box)
+                region.save(out_file_path)
+            else:
+                im.save(out_file_path)
--- utils.py
+++ utils.py
@@ -1,10 +1,94 @@
 import os
 from PIL import Image
-
-
-def read_dictionary(folder):
+ 
+# 读取目录下所有匹配文件
+def read_dictionary(folder, keys):
     images = []
-    for fileName in os.listdir(folder):
-        image = Image.open(folder + "/" + fileName)
-        images.append(image)
+    for file_name in os.listdir(folder):
+        flag = False
+        for key in keys:
+            if file_name.find(key) > -1:
+                flag = True
+        if flag:
+            image = Image.open(folder + "/" + file_name)
+            images.append(image)
     return images
+  
+# 删除目录下所有文件
+def del_dictionary_file(folder):
+    del_list = os.listdir(folder)
+    for f in del_list:
+        file_path = os.path.join(folder, f)
+        if os.path.isfile(file_path):
+            os.remove(file_path)
+
+def make_sprites(keys = [],ignoreStr=''):
+    folderPath = "./resize&group"
+    resultPath = "./css-sprites"
+    if not os.path.exists(resultPath):
+        os.makedirs(resultPath)
+    # 读取文件夹下的所有图片
+    images = []  # 图片
+    imagesName = []
+    images = read_dictionary(folderPath,keys)
+    for root, dirs, files in os.walk(folderPath):
+        for file_name in files:
+            flag = False
+            for key in keys:
+                if file_name.find(key) > -1:
+                    flag = True
+            if flag:
+                imagesName.append(file_name)
+    for i in range(len(imagesName)):
+        imagesName[i] = os.path.split(imagesName[i])[1]
+        imagesName[i] = os.path.splitext(imagesName[i])[0]
+    # 设置css sprite图片相关参数
+    # print("css sprite相关参数:")
+    p = pow(len(images), 0.5)
+    row = int(p) + 1
+    column = int(p)
+    # zoom = input("图片缩放大小（倍数）:")
+    # if zoom == "":
+    zoom = "0.5"
+    zoom = float(zoom)
+    # 个性化配置
+    # ignoreStr = input("需要忽略的文件中的字符串:")
+
+    # 计算css sprite大小
+    totalSize = (images[0].size[0] * column, images[0].size[1] * row)
+    resultImg = Image.new("RGBA", (int(totalSize[0] * zoom), int(totalSize[1] * zoom)), (0, 0, 0, 0))
+    resultStyle = "{\n"
+    # 获取图片高宽（每张图应该一致）
+    singleWidth = int(images[0].size[0] * zoom)
+    singleHeight = int(images[0].size[1] * zoom)
+    # 生成css sprite
+    widthPos = 0
+    heightPos = 0
+    i = -1
+
+    for image in images:
+        i = i + 1
+        # 添加图片
+        image = image.resize((int(image.size[0] * zoom), int(image.size[1] * zoom)))
+        resultImg.paste(image, (widthPos, heightPos, widthPos + image.size[0], heightPos + image.size[1]))
+        # 添加对应scss样式
+        imagesName[i] = imagesName[i].replace(ignoreStr, "")
+        # [x]改为_x，防止css样式问题
+        imagesName[i] = imagesName[i].replace("[", "_")
+        imagesName[i] = imagesName[i].replace("]", "")
+        resultStyle += "\t\"" + imagesName[i] + "\": [" + str(widthPos * -1) + "," + str(heightPos * -1)
+        # 切换到下一张图片
+        widthPos += image.size[0]
+        if i % column == column - 1:
+            heightPos += singleHeight
+            widthPos = 0
+        if i < len(images) - 1:
+          resultStyle += "],\n"
+        else:
+          resultStyle += "]\n"
+    resultStyle += "}"
+    resultImg.save(resultPath + '/' + keys[0] + str(zoom) + ".png")  # 写入图片
+    # 写入json
+    styleFile = open(resultPath + '/' + keys[0] + str(zoom) + ".json", "w")
+    styleFile.write(resultStyle)
+    styleFile.close()
